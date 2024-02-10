a9683c2627284b0847e69417b6f7dfc5b070f381
lens101 <robertharbord@hotmail.com>
2024-02-10 16:35:12 +0000

    create server function folder

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,14 +5,43 @@ import React, { FormEvent } from 'react';
 
 import { useState } from 'react';
 
-const isValidAmazonUrl = (url: string) => {};
+const isValidAmazonUrl = (url: string) => {
+  try {
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname; //https://www.amazon.co.uk/a-product-here
+
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon.') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+  return false;
+};
 
 const Searchbar = () => {
   const [searchParam, setSearchParam] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(searchParam);
+    const isValidLink = isValidAmazonUrl(searchParam);
+    if (!isValidLink) {
+      alert('Invalid Amazon URL');
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      //scrape the product
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -23,8 +52,12 @@ const Searchbar = () => {
         className="searchbar-input"
         onChange={(e) => setSearchParam(e.target.value)}
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchParam === ''}
+      >
+        {isLoading ? 'Searching' : 'Search'}
       </button>
     </form>
   );
