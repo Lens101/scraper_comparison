6f41fe6ff7c9b9fe972a4402c0d7fdb744eca6f5
lens101 <robertharbord@hotmail.com>
2024-02-15 21:43:42 +0000

    Product details page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import { getProductById } from '@/lib/actions';
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { formatNumber } from '@/lib/utils';
 
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product = await getProductById(id);
+  if (!product) redirect('/');
+
+  return (
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image
+            src={product.image}
+            width={580}
+            height={400}
+            alt={product.title}
+            className="mx-auto"
+          />
+        </div>
+
+        <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
+          <div className="flex flex-col gap-3">
+            <p className="text-[28px] text-secondary font-semibold">
+              {product.title}
+            </p>
+
+            <Link
+              href={product.url}
+              target="_blank"
+              className="text-base text-black opacity-50"
+            >
+              Visit Product
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="product-hearts">
+              <Image
+                src="/assets/icons/red-heart.svg"
+                alt="heart"
+                width={20}
+                height={20}
+              />
+
+              <p className="text-base font-semibold text-[#D46F77]">
+                {product.reviewsCount}
+              </p>
+            </div>
+
+            <div className="p-2 bg-white-200 rounded-10">
+              <Image
+                src="/assets/icons/bookmark.svg"
+                alt="bookmark"
+                width={20}
+                height={20}
+              />
+            </div>
+
+            <div className="p-2 bg-white-200 rounded-10">
+              <Image
+                src="/assets/icons/share.svg"
+                alt="share"
+                width={20}
+                height={20}
+              />
+            </div>
+
+            <div className="product-info">
+              <div className="flex flex-col gap-2">
+                <p className="text-[34px] text-secondary font-bold">
+                  {product.currency} {formatNumber(product.currentPrice)}
+                </p>
+                {/* If we have a discount it will show here. */}
+                <p className="text-[21px] text-black opacity-50 line-through">
+                  {product.currency} {formatNumber(product.originalPrice)}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
